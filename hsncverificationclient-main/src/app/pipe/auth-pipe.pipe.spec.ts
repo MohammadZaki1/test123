59ff84fc19a5ec2bd8d860c654d8c8f40bab45d9
MohammadZaki1 <mohammadzakinepa@gmail.com>
2025-03-13 13:44:18 +0530

    Delete hsncverificationclient-main directory

--- hsncverificationclient-main/src/app/pipe/auth-pipe.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { AuthPipePipe } from './auth-pipe.pipe';
-
-describe('AuthPipePipe', () => {
-  it('create an instance', () => {
-    const pipe = new AuthPipePipe();
-    expect(pipe).toBeTruthy();
-  });
-});
